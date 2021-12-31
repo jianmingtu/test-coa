a02d13530a76e1dc7c79cd78706549231eb8fb83
ekwan <224302@nttdata.com>
2021-12-31 10:07:33 -0800

    add swagger and get it to run

--- AutomatedTest/src/main/java/ca/bc/gov/open/coa/test/CoaTestRunner.java
+++ AutomatedTest/src/main/java/ca/bc/gov/open/coa/test/CoaTestRunner.java
@@ -10,4 +10,5 @@ public class CoaTestRunner {
         SpringApplication.run(CoaTestRunner.class, args);
     }
 
+
 }
--- AutomatedTest/src/main/java/ca/bc/gov/open/coa/test/controllers/TestController.java
+++ AutomatedTest/src/main/java/ca/bc/gov/open/coa/test/controllers/TestController.java
@@ -1,23 +1,29 @@
 package ca.bc.gov.open.coa.test.controllers;
 
-import ca.bc.gov.open.coa.test.services.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/tests")
+@RestController
+@RequestMapping("/tests")
 public class TestController {
-    private TestService testService;
+//    private TestService testService;
     @Autowired
-    public TestController(TestService testService){
+    public TestController(){
 
-        this.testService = testService;
+        //this.testService = testService;
     }
-    @GetMapping("/all")
+//    @GetMapping("/all")
+//
+//    public String runAllTests() throws Exception {
+//        testService.runAllTests();
+//        return"TestComplete";
+//
+//    }
 
-    public String runAllTests() throws Exception {
-        testService.runAllTests();
-        return"TestComplete";
-
+    @GetMapping("/ping")
+    public String ping() {
+        return "ping";
     }
 }
