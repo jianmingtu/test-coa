c76606312752909fc3a23eea84d6c339b11c2c68
ekwan <224302@nttdata.com>
2021-12-31 09:18:15 -0800

    add missing files

---
+++ AutomatedTest/src/main/java/ca/bc/gov/open/coa/test/controllers/TestController.java
@@ -0,0 +1,23 @@
+package ca.bc.gov.open.coa.test.controllers;
+
+import ca.bc.gov.open.coa.test.services.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/tests")
+public class TestController {
+    private TestService testService;
+    @Autowired
+    public TestController(TestService testService){
+
+        this.testService = testService;
+    }
+    @GetMapping("/all")
+
+    public String runAllTests() throws Exception {
+        testService.runAllTests();
+        return"TestComplete";
+
+    }
+}
