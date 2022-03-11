8e7d8e64c5a97a72ead72986b8e07c6e72661f46
Sylar Z. Zhang <Sylar.Zhang@gov.bc.ca>
2022-03-11 13:08:49 -0800

    added missing configs

--- AutomatedTest/src/main/java/ca/bc/gov/open/coa/test/config/SwaggerConfig.java
+++ AutomatedTest/src/main/java/ca/bc/gov/open/coa/test/config/SwaggerConfig.java
@@ -1,22 +1,25 @@
 package ca.bc.gov.open.coa.test.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("ca.bc.gov.open.coa.test"))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI api() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "basicAuth",
+                                        new SecurityScheme()
+                                                .name("basicAuth")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("basic")));
     }
 }
