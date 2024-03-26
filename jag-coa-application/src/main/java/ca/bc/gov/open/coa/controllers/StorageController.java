1e14ecf29a136dba5b2d0c33d0b3cae700f2ff63
jianming tu <tu_jm@yahoo.com>
2024-03-26 09:09:52 -0700

    keep -Xmx512m which is used in TEST and PROD; remove logging

--- jag-coa-application/src/main/java/ca/bc/gov/open/coa/controllers/StorageController.java
+++ jag-coa-application/src/main/java/ca/bc/gov/open/coa/controllers/StorageController.java
@@ -130,12 +130,9 @@ public class StorageController {
             throws JsonProcessingException {
         addEndpointHeader("StoreDocumentResponse");
 
-        logMemory();
-
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(host + "doc");
         HttpEntity<StoreDocumentBody> payload =
                 new HttpEntity<>(new StoreDocumentBody(search, coaConfig), new HttpHeaders());
-        logMemory();
 
         try {
             HttpEntity<StoreDocumentResponse> resp =
@@ -169,17 +166,5 @@ public class StorageController {
         } catch (Exception ex) {
             log.warn("Failed to add endpoint response header");
         }
-    }
-
-    void logMemory() {
-        log.info(
-                "Used Memory   :  "
-                        + (Runtime.getRuntime().totalMemory()
-                        - Runtime.getRuntime().freeMemory())
-                        + " bytes");
-        log.info("Free Memory   : " + Runtime.getRuntime().freeMemory() + " bytes");
-        log.info("Total Memory  : " + Runtime.getRuntime().totalMemory() + " bytes");
-        log.info("Max Memory    : " + Runtime.getRuntime().maxMemory() + " bytes");
-        log.info("------");
     }
 }
